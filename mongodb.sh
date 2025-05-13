a773c2498d9d5bfbcde0119c655cc2e4ca47fbfc
abhijeet4022 <abhijeet4022@gmail.com>
2025-05-13 09:22:14 +0530

    MongoDB Component

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,31 @@
 # MongoDB Installation and Configuration Script
+source ./common.sh
 LOG_FILE="/tmp/roboshop.log"
 component="mongodb"
 
+
+# Clear log file
+> "$LOG_FILE"
+
+# Install and Configure Nginx
+log_message "Configuring MongoDB repository..."
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> "$LOG_FILE"
+check_status "Repo Configuration"
+
+# Install MongoDB
+log_message "Installing MongoDB..."
+dnf install mongodb-org -y
+check_status "MongoDB installation"
+
+# Update MongoDB configuration
+log_message "Updating MongoDB configuration..."
+sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/mongod.conf &>> "$LOG_FILE"
+check_status "MongoDB configuration update"
+
+# Restart MongoDB service
+log_message "Restarting MongoDB service..."
+systemctl enable mongod &>> "$LOG_FILE"
+systemctl restart mongod &>> "$LOG_FILE"
+check_status "MongoDB service restart"
 
 
