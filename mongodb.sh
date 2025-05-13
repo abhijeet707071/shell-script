d1234427c5c1eed24b4d53039b22f95e76335c98
abhijeet4022 <abhijeet4022@gmail.com>
2025-05-13 09:28:15 +0530

    MongoDB Component

--- mongodb.sh
+++ mongodb.sh
@@ -8,25 +8,23 @@ component="mongodb"
 > "$LOG_FILE"
 
 # Install and Configure Nginx
-log_message "Configuring MongoDB repository..."
+log_message "Configuring MongoDB repository..." | tee -a "$LOG_FILE"
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> "$LOG_FILE"
 check_status "Repo Configuration"
 
 # Install MongoDB
-log_message "Installing MongoDB..."
-dnf install mongodb-org -y
-dnf install bash-completion -y
+log_message "Installing MongoDB..." | tee -a "$LOG_FILE"
+dnf install mongodb-org -y &>> "$LOG_FILE"
+dnf install bash-completion -y &>> "$LOG_FILE"
 check_status "MongoDB installation"
 
 # Update MongoDB configuration
-log_message "Updating MongoDB configuration..."
+log_message "Updating MongoDB configuration..." | tee -a "$LOG_FILE"
 sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/mongod.conf &>> "$LOG_FILE"
 check_status "MongoDB configuration update"
 
 # Restart MongoDB service
-log_message "Restarting MongoDB service..."
+log_message "Restarting MongoDB service..." | tee -a "$LOG_FILE"
 systemctl enable mongod &>> "$LOG_FILE"
 systemctl restart mongod &>> "$LOG_FILE"
 check_status "MongoDB service restart"
-
-
